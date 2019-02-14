029c8974afc1191aacd789c9432b33bb95c131f0
Xiangyu Li <47231543+XiangyuLiCCA@users.noreply.github.com>
2019-02-14 14:31:47 -0800

    Update speedy-lawnmower.js

--- speedy-lawnmower.js
+++ speedy-lawnmower.js
@@ -11,17 +11,17 @@ function draw() {
   line(x, height - 10, x + random(-10, 10), height - 10 - random(h)); //lawn fill
   noStroke();
 
-  x = x + 10; //speed of growth
+  x = x + 10; //speed of growth from left to right
 
   if (x > width) {
-    x = random(10);
-    h = h + 3;
+    x = random(10);//when lawn goes back, lawn will growth in 1-9
+    h = h + 3; //speed of growth
   }
 
   if (h >= 50) {
     fill(255);
     rect(0, 0, width, height - 30);
-    h = 30; // Lawn mower if lawn height > 150
+    h = 30; // Lawn mower if lawn height > 50
   }
 
   fill(40, 100, 60); //land color
